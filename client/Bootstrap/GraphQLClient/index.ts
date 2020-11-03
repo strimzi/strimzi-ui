bbb6389ec8b2bb1b698fe2744dfb7e8b2ab34a18
Jordan <jordan.tucker1@ibm.com>
2020-11-03 16:36:31 +0000

    fix: ignore coverage
    Signed-off-by: Jordan <jordan.tucker1@ibm.com>

--- client/Bootstrap/GraphQLClient/index.ts
+++ client/Bootstrap/GraphQLClient/index.ts
@@ -2,4 +2,7 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-export { apolloClient } from './GraphQlClient';
+
+/* istanbul ignore file */
+
+export { apolloClient } from './GraphQLClient';
